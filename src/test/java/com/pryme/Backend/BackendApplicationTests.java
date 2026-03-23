e25c94c614b81dcdae2c70bd7fbd882191af63df
ankitmehtacode <everytingcreativehere@gmail.com>
2026-03-23 12:03:32 +0530

    feat: Add test annotation to BackendApplicationTests.java

--- src/test/java/com/pryme/Backend/BackendApplicationTests.java
+++ src/test/java/com/pryme/Backend/BackendApplicationTests.java
@@ -2,6 +2,7 @@ package com.pryme.Backend;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.junit.jupiter.api.Test;
 
 @SpringBootTest
 @TestPropertySource(properties = {
